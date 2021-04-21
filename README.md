9655049d74149a03bdc0e542d2b5e7dc80b02614
Miguel <MiguelBG99@hotmail.com>
2021-04-21 20:50:25 +0200

    Auto-generated commit

--- README.md
+++
@@ -1,6 +0,0 @@
-# SuperHeroApp
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.9.
-
-## github pages
-https://mbg999.github.io/hero-app-angular
--- src/app/directives/to-upper-case/to-upper-case.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToUpperCaseDirective } from './to-upper-case.directive';
-
-describe('ToUpperCaseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ToUpperCaseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
